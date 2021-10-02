aeacdbc33508daf1e8ef26d347c78d96d2ebbb71
Amit-Choudhary11 <74827051+Amit-Choudhary11@users.noreply.github.com>
2021-10-02 09:00:52 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,36 @@
 
-function setup(){
-canvas= createCanvas(390,300);
-canvas.center();
+function preload() {
+    
+}
 
+function setup(){
+canvas=createCanvas(390,300);
+canvas.position(550,370);
+video=createCapture(VIDEO);
+video.size(390,300);
+video.hide();
+
+classifier= ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/XKf83xZZC/model.json",modelLoaded);
+}
+
+function modelLoaded() {
+  console.log("model Loaded");
+}
+
+function draw(){
+image(video,0,0,390,300);
+classifier.classify(video,gotResults);
 }
 
 
-function draw(){
+function gotResults(error,results){
+    if(error){
+        console.error(error);
+    }else{
+        console.log(results);
+        document.getElementById("person").innerHTML=results[0].label;
+        
+        document.getElementById("accuracy").innerHTML=results[0].confidence.toFixed(3);
+
+    }
 }
